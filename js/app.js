5a7f953247112088db8d88b7461d31dc23d96854
Vamsi Batchu <vamsibatchuk@gmail.com>
2018-03-28 11:00:02 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#ffffff"
+        "value": "#CCC"
       },
       "shape": {
         "type": "circle",
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#ffffff",
+        "color": "#CCC",
         "opacity": 0.4,
         "width": 1
       },
